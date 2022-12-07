00b8f4515bfe58b8833fc019fa190af7748b4c21
Łukasz Kolber <kolberu@gmail.com>
2022-12-07 11:20:40 +0100

    Documentation

--- src/led/led.py
+++ src/led/led.py
@@ -3,11 +3,22 @@ from utime import sleep_us
 
 
 class Led(BaseLed):
+    """
+    Simple Led class for managing single led element.
+    """
     def __init__(self, pin, frequency=1000, default_duty=0, default_blink_time_ms=500, interval_break_us=1500):
         super().__init__(pin, frequency, default_duty, interval_break_us)
         self.default_blink_time_ms = default_blink_time_ms
 
     def blink(self, n=1, value=None, blink_time_ms=None):
+        """
+        Blinks led. If led is on, method first disables led then blinks and after blinks automatically turns led on
+        with previous value (Total time needed is extended by half of blink_time_ms or default_blink_time_ms).
+
+        :param n: Number of blinks.
+        :param value: Blink glow.
+        :param blink_time_ms: Time for one blink.
+        """
 
         if blink_time_ms is not None:
             timespan = self._calc_span_us(blink_time_ms, 2)
@@ -26,6 +37,12 @@ class Led(BaseLed):
         self._led.duty_u16(self._duty)
 
     def _blink(self, timespan, duty):
+        """
+        Blinks led.
+
+        :param timespan: Half of total blink time.
+        :param duty: Led glow.
+        """
         self._led.duty_u16(duty)
         sleep_us(timespan)
         self._led.duty_u16(0)
