c6524a7fb8af79322a746c2646d8ece94222a800
Łukasz Kolber <69080157+psp515@users.noreply.github.com>
2022-12-07 11:24:49 +0100

    Led (#26)
    * LedSetup
    
    * Update README.md
    
    * refactorStart
    
    * WorkingLed
    
    * photorezistor fix
    
    * Examples
    
    * ReadMeUpdates
    
    * Documentation
    
    * ExampleExtension

--- src/interfaces/adc_sensor.py
+++ src/interfaces/adc_sensor.py
@@ -1,7 +1,7 @@
 from machine import ADC, Pin
 
-from exceptions.invalid_pin_exception import InvalidPinException
-from rpip_const import RPIP_ADC_PINS, RPIP_MAX_READ
+from src.exceptions.invalid_pin_exception import InvalidPinException
+from src.const import ADC_PINS, MAX_ADC
 
 
 class ADCSensor:
@@ -11,10 +11,12 @@ class ADCSensor:
     def __init__(self, adc_pin):
         """
         Initializes ADC sensor.
-        :param adc_pin: ADC pin.
+
+        :param adc_pin: Pin with ADC for sensor.
+        :raises InvalidPinException: When invalid pin provided.
         """
 
-        if(adc_pin not in RPIP_ADC_PINS):
+        if adc_pin not in ADC_PINS:
             raise InvalidPinException(f"Provided pin ({adc_pin}) is not a ADC pin.")
 
         self._adc_sensor = ADC(Pin(adc_pin))
@@ -23,15 +25,17 @@ class ADCSensor:
     def max_read_value(self):
         """
         Returns maximal possible value that adc pin can return.
-        :return: Maximal read pin value.
+
+        :returns: Maximal read pin value.
         """
-        return RPIP_MAX_READ
+        return MAX_ADC
 
     def read_percent(self, precision=2):
         """
         Returns value on analog pin in percent.
+
         :param precision: Precision of retruned value.
-        :return: Read value in percent.
+        :returns: Read value in percent.
         """
         return round(self._adc_sensor.read_u16() / self.max_read_value * 100, precision)
 
