dbf3c0bb43ace0cd192980ca0a4d0e6c1fbc997f
Łukasz Kolber <69080157+psp515@users.noreply.github.com>
2022-12-10 15:13:09 +0100

    Potentiometer (#29)
    * potentiometer setup
    
    * Update README.md
    
    * Update README.md

--- micropico.py
+++ micropico.py
@@ -2,6 +2,7 @@ from src.keypad.keypad import Keypad
 from src.led.led import Led
 from src.photorezistor.photorezistor import Photorezistor
 from src.ultrasonic.ultrasonic import Ultrasonic
+from src.potentiometer.potentiometer import Potentiometer
 
 # Tools
 from src.tools.length_units_enum import *
